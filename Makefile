9a832b522922c98040c9dbc1f4afb29b70e3a4e1
Elliot Gibeaux <egibeaux@c2r5p4.42lehavre.fr>
2025-03-06 23:19:37 +0100

    added makefile and srcdirs

---
+++ Makefile
@@ -0,0 +1,36 @@
+NAME = cub3d
+CFLAGS = -Wall -Wextra -Werror -g
+LIB = libft/libft.a
+FT_PRINTF = ft_printf/ft_printf.a
+SRCDIR = sources
+SRC = $(SRCDIR)/minishell.c \
+
+OBJ := $(SRC:.c=.o)
+
+all: $(NAME)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME): $(OBJ) $(LIB) $(FT_PRINTF)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJ) $(FT_PRINTF) $(LIB)
+
+$(LIB):
+	$(MAKE) -C libft
+
+$(FT_PRINTF):
+	$(MAKE) -C ft_printf
+
+clean:
+	$(MAKE) clean -C libft
+	$(MAKE) clean -C ft_printf
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+	$(MAKE) fclean -C libft
+	$(MAKE) fclean -C ft_printf
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egibeaux <egibeaux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 23:11:51 by egibeaux          #+#    #+#             */
+/*   Updated: 2025/03/06 23:14:14 by egibeaux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "libft/libft.h"
+#include "ft_printf/ft_printf.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <stdbool.h>
+
+#endif
